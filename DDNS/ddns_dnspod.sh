e34e6afefc6cf4d47a419f9f44b0f2f62b320db7
tanst <toyst@163.com>
2019-03-15 15:50:05 +0800

    Update ddns_dnspod.sh

--- DDNS/ddns_dnspod.sh
+++ DDNS/ddns_dnspod.sh
@@ -1,115 +1,88 @@
 #!/bin/sh
 
 # personal config
-API_ID=12345
-API_Token=000000000000000000000
-SUBDOMAIN=ddns
-DOMAIN=abc.com
-Email=abc@abc.com
-CHECKURL=http://icanhazip.com/
+API_ID=71374
+API_Token=924617b61d423f1c3ee823d94753c2ce
+SUBDOMAIN=@
+DOMAIN=tanst.net
+Email=t@tanst.net
+CHECKURL=$(curl -s -4 icanhazip.com)
 
 # system config
 DNSPOD_TOKEN="login_token=${API_ID},${API_Token}"
-UPDATE_STATUS=0
 date
+cd $(dirname $0)
+
 # function
 dnspod_send_email(){
 
-	IP_CONF='./ip.conf'
-	if [ ! -d $IP_CONF ]
-	then
-		touch $IP_CONF
-	fi
-	CONF_IP=$(cat $IP_CONF)
-	
-	if [ "$CONF_IP" != "$CURRENT_IP" ] || [ $UPDATE_STATUS == 1 ]
-	then
-		curl -k https://www.xdty.org/mail/mail.php -X POST -d "event=CURRENT_IP:$CURRENT_IP changed&name=$SUBDOMAIN.$DOMAIN&email=$Email"
-		echo "$CURRENT_IP">$IP_CONF
-		echo "Email is sent successfully."
-	else
-		echo "IP NO CHANGED"
-	fi
+	curl -k https://tanst.net/script/mail.php -X POST -d "event=the New ip is: $CURRENT_IP&title=[K2P]The IP Changed! &email=$Email"
+	echo -e "\033[1;32;40mEmail is sent successfully."
 	exit
 }
 
-
-# dnspod_ip_check
-
-## DNS_IP = CURRENT_IP
-	IPREX='([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
-	CURRENT_IP=$(curl -s $CHECKURL|grep -Eo "$IPREX"|tail -n1)
-	echo "CURRENT_IP:$CURRENT_IP"
-
-	DNSCMD="nslookup";type nslookup >/dev/null 2>&1||DNSCMD="ping -c1"
-	DNSTEST=$($DNSCMD $SUBDOMAIN.$DOMAIN)
-	if [ "$?" == 0 ]
-	then DNS_IP=$(echo $DNSTEST|grep -Eo "$IPREX"|tail -n1)
-	else DNS_IP="Get $domain DNS Failed."
-	fi
-	echo "DNS_IP:$DNS_IP"
-
-	if [ "$DNS_IP" == "$CURRENT_IP" ]
+CURRENT_IP() {
+    
+	CURRENT_IP=$CHECKURL
+	if [ $CURRENT_IP ]
 	then
-		echo "DNS_IP = CURRENT_IP , SKIP UPDATE."
-		dnspod_send_email
+	echo "CURRENT_IP : $CURRENT_IP"
+	else
+	echo "获取公网IP错误，请检查URL是否正常"
+	exit
+	fi
+	
+}
+
+dnspod_record_ip() {
+
+	Record_List="$(curl -s -k https://dnsapi.cn/Record.List -d "${DNSPOD_TOKEN}&format=json&record_type=A&domain=${DOMAIN}&sub_domain=${SUBDOMAIN}")"
+
+	RECORD_IP=$(echo $Record_List | sed 's/.*,"value":"\([0-9\.]*\)".*/\1/')
+	LINE_ID=$(echo $Record_List | sed 's/.*,"line_id":"\([0-9]*\)".*/\1/')
+	STATUS_CODE=$(echo $Record_List | sed 's/.*{"code":"\([0-9]*\)".*/\1/')
+	RECORD_ID=$(echo $Record_List | sed 's/.*\[{"id":"\([0-9]*\)".*/\1/')
+
+	#echo "RECORD_IP: $RECORD_IP";
+	#echo "RECORD_ID: $RECORD_ID";
+	#echo "LINE_ID: $LINE_ID";
+	#echo "STATUS_CODE: $STATUS_CODE";
+
+	if [ "$STATUS_CODE" != "1" ]
+	then echo "Error:Get DNSPOD API Error!";exit
 	fi
 
-## RECORD_IP = CURRENT_IP
+	echo "RECORD_IP : $RECORD_IP"
 
-	Record_List="$(curl -s -k https://dnsapi.cn/Record.List -d "${DNSPOD_TOKEN}&domain=${DOMAIN}&sub_domain=${SUBDOMAIN}")"
+}
 
-    for line in $Record_List;do
-        if [ $(echo $line|grep '<value>' |wc -l) != 0 ];then
-            RECORD_IP=${line%<*};
-            RECORD_IP=${RECORD_IP#*>};
-            #echo "RECORD_IP: $RECORD_IP";
-        fi
-        if [ $(echo $line|grep '<id>' |wc -l) != 0 ];then
-            RECORD_ID=${line%<*};
-            RECORD_ID=${RECORD_ID#*>};
-            #echo "RECORD_ID: $RECORD_ID";
-        fi
-        if [ $(echo $line|grep '<line_id>' |wc -l) != 0 ];then
-            LINE_ID=${line%<*};
-            LINE_ID=${LINE_ID#*>};
-            #echo "LINE_ID: $LINE_ID";
-        fi
-        if [ $(echo $line|grep '<code>' |wc -l) != 0 ];then
-            STATUS_CODE=${line%<*};
-            STATUS_CODE=${STATUS_CODE#*>};
-            #echo "STATUS_CODE: $STATUS_CODE";
-        fi
-    done
-
-if [ "$STATUS_CODE" != "1" ]
-then echo "Error:Get DNSPOD API Error!";exit
-fi
-
-	echo "RECORD_IP:$RECORD_IP"
+dnspod_update() {
 
 	if [ "$RECORD_IP" == "$CURRENT_IP" ]
 	then
 		echo "RECORD_IP = CURRENT_IP , SKIP UPDATE."
-		dnspod_send_email
+		exit
 	fi
 
-# dnspod_update
+	echo Start DDNS update...
 
-echo Start DDNS update...
+	Record_Ddns="$(curl -s -k -X POST https://dnsapi.cn/Record.Ddns -d "${DNSPOD_TOKEN}&record_id=${RECORD_ID}&record_line_id=${LINE_ID}&domain=${DOMAIN}&sub_domain=${SUBDOMAIN}")"
 
-Record_Ddns="$(curl $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s -k -X POST https://dnsapi.cn/Record.Ddns -d "${DNSPOD_TOKEN}&record_id=${RECORD_ID}&record_line_id=${LINE_ID}&domain=${DOMAIN}&sub_domain=${SUBDOMAIN}")"
+	DDNS_RESULT=${Record_Ddns#*\<message\>};
+	DDNS_RESULT=${DDNS_RESULT%%\</message\>*};
+	echo -e "\033[1;32;40m$DDNS_RESULT"
+	#echo -e "\033[字背景颜色;字体颜色m 字符串 \033[0m" 
+	for line in $Record_Ddns;do
+		if [ $(echo $line|grep '<value>' |wc -l) != 0 ];then
+			Finale_IP=${line%<*};
+			Finale_IP=${Finale_IP#*>};
+			echo -e "\033[0mFinale_IP: \033[1;32;40m$Finale_IP";
+		fi
+	done
+	dnspod_send_email
 
-DDNS_RESULT=${Record_Ddns#*\<message\>};
-DDNS_RESULT=${DDNS_RESULT%%\</message\>*};
-echo -n -e "\033[1;32;40mCongratulations! \033[0m$DDNS_RESULT"
-#echo -e "\033[字背景颜色;字体颜色m 字符串 \033[0m" 
-for line in $Record_Ddns;do
-    if [ $(echo $line|grep '<value>' |wc -l) != 0 ];then
-        Finale_IP=${line%<*};
-        Finale_IP=${Finale_IP#*>};
-        echo "Finale_IP: $Finale_IP";
-    fi
-done
-UPDATE_STATUS=1
-dnspod_send_email
+}
+
+CURRENT_IP
+dnspod_record_ip
+dnspod_update
