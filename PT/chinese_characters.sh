e0e0bcb67c22513b23c697a11e938d7ffc9a4a04
tanst <t@tanst.net>
2021-05-26 12:58:14 +0800

    Update chinese_characters.sh

--- PT/chinese_characters.sh
+++ PT/chinese_characters.sh
@@ -18,35 +18,36 @@ curl -s -k "${HOST}/api/v2/torrents/delete?hashes=${DELETE_HASHES}&deleteFiles=t
 ## 清除【movie】和【TV】分类文件名中的中文字段 ##
 clear_chinese_char() {
 
-	FILES_LIST="$(curl -s -k "${HOST}/api/v2/torrents/files?hash=${HASH}" --cookie "SID=$COOKIE" | sed "s/},{/}\n{/g" | sed 's/.*"name":"\(.*\)","piece_range".*/\1/g')"
-	echo "${FILES_LIST}" >/dev/shm/FILESLIST$$
-	while read oldPath; do
-		fen=$(echo ${oldPath} | sed 's/\//\/\n/g')
-		for line in ${fen}; do
-			NOCHANGE=$(echo ${line} | grep -Ev '.txt|.nfo|.jpg|.png|.ini')
-			if [ -z ${NOCHANGE} ]; then
-				# 如果是文本图片文件，则执行以下命令
-				echo "${line}" >>/dev/shm/TEST$$
-			else
-				# 开始去中文
-				line2=$(echo "${line}" | sed 's/\./\n/g' | sed '/[^A-Za-z0-9 -/@_&￡]/d' | sed ':a;N;s/\n/./g;ta')
-				echo "${line2}" >>/dev/shm/TEST$$
-			fi
+    FILES_LIST="$(curl -s -k "${HOST}/api/v2/torrents/files?hash=${HASH}" --cookie "SID=$COOKIE" | sed "s/},{/}\n{/g" | sed 's/.*"name":"\(.*\)","piece_range".*/\1/g')"
+    echo "${FILES_LIST}" >/dev/shm/FILESLIST$$
+    while read oldPath; do
+        fen=$(echo ${oldPath} | sed 's/\//\/\n/g')
+        for line in ${fen}; do
+            NOCHANGE=$(echo ${line} | grep -Ev '.txt|.nfo|.jpg|.png|.ini')
+            if [ -z ${NOCHANGE} ]; then
+                # 如果是文本图片文件，则执行以下命令
+                echo "${line}" >>/dev/shm/TEST$$
+            else
+                # 开始去中文
+                line2=$(echo "${line}" | sed 's/\./\n/g' | sed '/[^A-Za-z0-9 -/@_&￡]/d' | sed ':a;N;s/\n/./g;ta')
+                echo "${line2}" >>/dev/shm/TEST$$
+            fi
 
-		done
-		newPath=$(sed ':a;N;s/\/\n/\//g;ta' /dev/shm/TEST$$)
+        done
+        newPath=$(sed ':a;N;s/\/\n/\//g;ta' /dev/shm/TEST$$)
         curl -s -k -G --data-urlencode "oldPath=$oldPath" --data-urlencode "newPath=$newPath" "$HOST/api/v2/torrents/renameFile?hash=$HASH" --cookie "SID=$COOKIE"
-		echo "=============================="
-		echo "oldPath : $oldPath"
-		echo "newPath : $newPath"
-		rm /dev/shm/TEST$$
-	done </dev/shm/FILESLIST$$
-	rm /dev/shm/FILESLIST$$
+        echo "=============================="
+        echo "oldPath : $oldPath"
+        echo "newPath : $newPath"
+        rm /dev/shm/TEST$$
+    done </dev/shm/FILESLIST$$
+    rm /dev/shm/FILESLIST$$
 
 }
 RESULT=$(echo "${CATEGORY}" | grep -E 'TV|movie')
 if [ -z ${RESULT} ]; then
-	exit
+    exit
 else
-	clear_chinese_char
+    clear_chinese_char
 fi
+
