d94a9ae6908f7ca9d92dc8e6a9b0ce1dc38e51b5
tanst <t@tanst.net>
2021-05-31 14:07:25 +0800

    Update ssh_key.sh

--- ssh_key.sh
+++ ssh_key.sh
@@ -3,7 +3,8 @@
 ###################################
 #    bash <(curl -Lso- https://raw.githubusercontent.com/tanst/scripts/master/ssh_key.sh)
 ###################################
-
+apt-get update -y
+apt-get install curl -y
 echo "PS1='\[\e[1;32;40m\][\u@\h - \t \W]\$ \[\e[0m\]'" >> ~/.bashrc
 echo "alias ll='ls --color=auto -l'" >> ~/.bashrc
 echo "alias ls='ls --color=auto'" >> ~/.bashrc
@@ -11,15 +12,13 @@ mkdir ~/.ssh
 curl -s https://github.com/tanst.keys > ~/.ssh/authorized_keys
 chmod 700 ~/.ssh/authorized_keys
 chmod 600 ~/.ssh
-sed -i "/PasswordAuthentication no/c PasswordAuthentication no" /etc/ssh/sshd_config
-sed -i "/PasswordAuthentication yes/c PasswordAuthentication no" /etc/ssh/sshd_config
-sed -i "/PubkeyAuthentication no/c PubkeyAuthentication yes" /etc/ssh/sshd_config
-sed -i "/PubkeyAuthentication yes/c PubkeyAuthentication yes" /etc/ssh/sshd_config
-sed -i "/PrintMotd no/c PrintMotd yes" /etc/ssh/sshd_config
-sed -i "/PrintMotd yes/c PrintMotd yes" /etc/ssh/sshd_config
-sed -i "/PrintLastLog no/c PrintLastLog yes" /etc/ssh/sshd_config
-sed -i "/PrintLastLog yes/c PrintLastLog yes" /etc/ssh/sshd_config
-sed -i "/Port.*/c Port 43389" /etc/ssh/sshd_config
+sed -i "/PasswordAuthentication /c PasswordAuthentication no" /etc/ssh/sshd_config
+sed -i "/PubkeyAuthentication /c PubkeyAuthentication yes" /etc/ssh/sshd_config
+sed -i "/PrintMotd /c PrintMotd yes" /etc/ssh/sshd_config
+sed -i "/PrintLastLog /c PrintLastLog yes" /etc/ssh/sshd_config
+sed -i "/Port /c Port 43389" /etc/ssh/sshd_config
+service ssh restart
+systemctl restart ssh
 service sshd restart
 systemctl restart sshd
 echo -e "Please enter \033[0;35msource ~/.bashrc\033[0m for immediate effect"
