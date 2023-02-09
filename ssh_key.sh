0ace0c23bc84da94f9892d7548ff037bfcffebb7
tanst <t@tanst.net>
2023-02-09 14:21:44 +0800

    fix chmod

--- ssh_key.sh
+++ ssh_key.sh
@@ -10,15 +10,12 @@ echo "alias ll='ls --color=auto -l'" >> ~/.bashrc
 echo "alias ls='ls --color=auto'" >> ~/.bashrc
 mkdir -p ~/.ssh
 curl -s https://github.com/tanst.keys > ~/.ssh/authorized_keys
-chmod 700 ~/.ssh/authorized_keys
-chmod 600 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
+chmod 700 ~/.ssh
 sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/g' /etc/ssh/sshd_config
 sed -i 's/^#\?PubkeyAuthentication.*/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
 sed -i 's/^#\?PrintMotd.*/PrintMotd yes/g' /etc/ssh/sshd_config
 sed -i 's/^#\?PrintLastLog.*/PrintLastLog yes/g' /etc/ssh/sshd_config
 sed -i 's/^#\?Port.*/Port 43389/g' /etc/ssh/sshd_config
-service ssh restart
 systemctl restart ssh
-service sshd restart
-systemctl restart sshd
 echo -e "Please enter \033[0;35msource ~/.bashrc\033[0m for immediate effect"
