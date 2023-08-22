253a9d0156510090c872ad43a2cb0b46e6131538
tanst <t@tanst.net>
2023-08-22 13:03:09 +0800

    Update ssh_key.sh

--- ssh_key.sh
+++ ssh_key.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ###################################
-#    bash <(curl -Lso- https://raw.githubusercontent.com/tanst/scripts/master/ssh_key.sh)
+#    bash <(curl -Lso- https://ghproxy.com/https://raw.githubusercontent.com/tanst/scripts/master/ssh_key.sh)
 ###################################
 apt-get update -y
 apt-get install curl -y
@@ -9,7 +9,7 @@ echo "PS1='\[\e[1;33m\]\u\[\e[1;31m\]@\[\e[1;35m\]\h\[\e[1;32m\][\t]\[\e[1;31m\]
 echo "alias ll='ls --color=auto -l'" >> ~/.bashrc
 echo "alias ls='ls --color=auto'" >> ~/.bashrc
 mkdir -p ~/.ssh
-curl -s https://github.com/tanst.keys > ~/.ssh/authorized_keys
+curl -s https://ghproxy.com/https://github.com/tanst.keys > ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 chmod 700 ~/.ssh
 sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/g' /etc/ssh/sshd_config
