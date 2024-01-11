650062b46c6c52bc6c5cb6100656b5467de63a07
tanst <t@tanst.net>
2024-01-11 11:12:28 +0800

    Update ssh_key.sh

--- ssh_key.sh
+++ ssh_key.sh
@@ -3,8 +3,8 @@
 ###################################
 #    bash <(curl -Lso- https://raw.githubusercontent.com/tanst/scripts/master/ssh_key.sh)
 ###################################
-apt-get update -y
-apt-get install curl -y
+apt update -y
+ap install curl -y
 echo "PS1='\[\e[1;33m\]\u\[\e[1;31m\]@\[\e[1;35m\]\h\[\e[1;32m\][\t]\[\e[1;31m\]:\[\e[1;36m\]\w\[\e[1;34m\]\$\[\e[0;39m\] '" >> ~/.bashrc
 echo "alias ll='ls --color=auto -l'" >> ~/.bashrc
 echo "alias ls='ls --color=auto'" >> ~/.bashrc
@@ -17,5 +17,6 @@ sed -i 's/^#\?PubkeyAuthentication.*/PubkeyAuthentication yes/g' /etc/ssh/sshd_c
 sed -i 's/^#\?PrintMotd.*/PrintMotd yes/g' /etc/ssh/sshd_config
 sed -i 's/^#\?PrintLastLog.*/PrintLastLog yes/g' /etc/ssh/sshd_config
 sed -i 's/^#\?Port.*/Port 43389/g' /etc/ssh/sshd_config
+sed -i '/^X11Forwarding/s/^/#/' /etc/ssh/sshd_config
 systemctl restart ssh
 echo -e "Please enter \033[0;35msource ~/.bashrc\033[0m for immediate effect"
