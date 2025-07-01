9cc02228b2a6189a47266d7946cddf979a89ea17
tanst <t@tanst.net>
2025-07-01 17:19:04 +0800

    Update emby_Buttons.js

--- emby_Buttons.js
+++ emby_Buttons.js
@@ -4,7 +4,7 @@
 // @name:zh      embyLaunchPotplayer
 // @name:zh-CN   embyLaunchPotplayer
 // @namespace    http://tampermonkey.net/
-// @version      1.1.21
+// @version      1.1.22
 // @description  emby/jellfin launch extetnal player
 // @description:zh-cn emby/jellfin 调用外部播放器
 // @description:en  emby/jellfin to external player
@@ -351,7 +351,8 @@
             subs: subs,
             subs_name: subs_name,
             subs_filename: subs_filename,
-            subs_enable: subs_enable
+            subs_enable: subs_enable,
+            path: mediaSource.Path,
         };
     }
 
@@ -393,7 +394,7 @@
         let intent = mediaInfo.intent;
         // android subtitles:  https://code.videolan.org/videolan/vlc-android/-/issues/1903
         let vlcUrl = `intent:${encodeURI(mediaInfo.streamUrl)}#Intent;package=org.videolan.vlc;type=video/*;S.subtitles_location=${encodeURI(mediaInfo.subUrl)};S.title=${encodeURI(intent.title)};i.position=${intent.position};end`;
-        if (OS.isWindows()) {
+        if (OS.isWindows() || OS.isMacOS()) {
             // 桌面端需要额外设置,参考这个项目:
             // new: https://github.com/northsea4/vlc-protocol
             // old: https://github.com/stefansundin/vlc-protocol
@@ -406,6 +407,31 @@
         }
         console.log(vlcUrl);
         window.open(vlcUrl, "_self");
+    }
+
+    // MPV
+    async function embyMPV() {
+        let mediaInfo = await getEmbyMediaInfo();
+        // 桌面端需要额外设置,参考这个项目:
+        // new: https://github.com/northsea4/mpvplay-protocol
+        // old: https://github.com/akiirui/mpv-handler
+        let streamUrl64 = btoa(String.fromCharCode.apply(null, new Uint8Array(new TextEncoder().encode(mediaInfo.streamUrl))))
+            .replace(/\//g, "_").replace(/\+/g, "-").replace(/\=/g, "");
+        let MPVUrl = `mpv://play/${streamUrl64}`;
+        if (mediaInfo.subUrl.length > 0) {
+            let subUrl64 = btoa(mediaInfo.subUrl).replace(/\//g, "_").replace(/\+/g, "-").replace(/\=/g, "");
+            MPVUrl = `mpv://play/${streamUrl64}/?subfile=${subUrl64}`;
+        }
+
+        if (OS.isIOS() || OS.isAndroid()) {
+            MPVUrl = `mpv://${encodeURI(mediaInfo.streamUrl)}`;
+        }
+        if (OS.isMacOS()) {
+            MPVUrl = `mpvplay://${encodeURI(mediaInfo.streamUrl)}`;
+        }
+
+        console.log(MPVUrl);
+        window.open(MPVUrl, "_self");
     }
 
     // https://github.com/iina/iina/issues/1991
@@ -463,26 +489,6 @@
         let stellarPlayerUrl = `stellar://play/${encodeURI(mediaInfo.streamUrl)}`;
         console.log(`stellarPlayerUrl= ${stellarPlayerUrl}`);
         window.open(stellarPlayerUrl, "_self");
-    }
-
-    // MPV
-    async function embyMPV() {
-        let mediaInfo = await getEmbyMediaInfo();
-        //桌面端需要额外设置,使用这个项目: https://github.com/akiirui/mpv-handler
-        let streamUrl64 = btoa(String.fromCharCode.apply(null, new Uint8Array(new TextEncoder().encode(mediaInfo.streamUrl))))
-            .replace(/\//g, "_").replace(/\+/g, "-").replace(/\=/g, "");
-        let MPVUrl = `mpv://play/${streamUrl64}`;
-        if (mediaInfo.subUrl.length > 0) {
-            let subUrl64 = btoa(mediaInfo.subUrl).replace(/\//g, "_").replace(/\+/g, "-").replace(/\=/g, "");
-            MPVUrl = `mpv://play/${streamUrl64}/?subfile=${subUrl64}`;
-        }
-
-        if (OS.isIOS() || OS.isAndroid()) {
-            MPVUrl = `mpv://${encodeURI(mediaInfo.streamUrl)}`;
-        }
-
-        console.log(MPVUrl);
-        window.open(MPVUrl, "_self");
     }
 
     // see https://greasyfork.org/zh-CN/scripts/443916
